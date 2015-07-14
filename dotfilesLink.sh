257479b1eb4cf4adef01c4d5db468644fa5c27fe
tossh0095 <tossh0095@users.noreply.github.com>
2015-07-14 09:50:31 +0900

    add .vim directory to configuration script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s  ~/dotfiles/.vim ~/.vim
